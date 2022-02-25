e04c3813bddce757ef50aba7e2b3ca072e6fb6dd
Vladimir Yablokov <YablokovWork@gmail.com>
2022-02-25 20:23:51 +0300

    change JS, task1 v.1.1

--- Yablokov_Vladimir/script/script.js
+++ Yablokov_Vladimir/script/script.js
@@ -1,32 +1,33 @@
-//!!ЗАДАЧА ПЕРВАЯ!!//
+//!!ЗАДАЧА ПЕРВАЯ!!// 
+//v1.0
+const noMath = {
+    numbers : [3, 2, 1, 0, 4, 5, 6, 10, 9, 8, 7],
+    get_max(){
+        return this.numbers.sort((a, b) => b-a)[0]
+    },
+    get_min(){
+        return this.numbers.sort((a, b) => a-b)[0]
+    },
+    get_count(){
+        return this.numbers.length
+    },
+    get_avg(){
+        let numbSum = 0;
+        for(let i = 0; i < this.get_count(); i++){
+            numbSum += this.numbers[i];
+        };
+        return numbSum/this.get_count()
+    }
+}
+console.log(noMath.get_max());
+console.log(noMath.get_min());
+console.log(noMath.get_count());
+console.log(noMath.get_avg());
+
+//v1.1
 const numbers = [3, 2, 1, 0, 4, 5, 6, 10, 9, 8, 7];
-
-// Решение min/max 
-function maxN(numbers){
-	return numbers.sort((a,b) => a-b)[numbers.length-1]
-}
-function minN(numbers){
-	return numbers.sort((a,b) => a-b)[0]
-}
-// Решение count со значением кол-ва элементов в массиве
-function count(numbers){
-		return numbers.length
-}
-//avg со значением среднего 
-function average(numbers){
-	let numbSum = 0;
-for(let i = 0; i < numbers.length; i++){
-	numbSum += numbers[i];
-	}
-	let avg = numbSum/numbers.length;
-	return avg
-}
-console.log(maxN(numbers),minN(numbers),count(numbers),average(numbers));
-
-// сразу не увидел что надо сделать объект(вперед поезда мчу)
-
 const object = {};
-function noMath(numbers){
+function noMath1(numbers){
     let sort = numbers.sort((a, b) => a-b);
     object.max = sort[sort.length-1];
     object.min = sort[0];
@@ -38,15 +39,19 @@ function noMath(numbers){
     object.average = numbSum/numbers.length;
 return object;
 }
-noMath(numbers);
+noMath1(numbers);
 console.log(object);
 
-//!!ЗАДАЧА ВТОРАЯ!!//
+// хочу поинтересоваться какая реализация более правильная?
+// v1.0 делал опираясь на прошедший материал
+// v1.1 подсмотрел в интернете
 
+//!!ЗАДАЧА ВТОРАЯ!!//
 const formElem = document.querySelector('#numbForm');
 const inputElem = document.querySelector('#inNumb');
 const resultElem = document.querySelector('#result');
 let sum = 0;
+
 function formNumb(event){
 	event.preventDefault();
 	let numb = +inputElem.value
@@ -56,10 +61,10 @@ function formNumb(event){
 formElem.addEventListener('submit', formNumb);
 
 //!!ЗАДАЧА ТРЕТЬЯ!!//
-
 const colorForm = document.querySelector('#colorForm'); 
 const inputColorElem = document.querySelector('#inColor'); 
 const colorOut = document.querySelector('#colors'); 
+
 function formColor(event){
     event.preventDefault(); 
     let divColor = document.createElement('div'); 
